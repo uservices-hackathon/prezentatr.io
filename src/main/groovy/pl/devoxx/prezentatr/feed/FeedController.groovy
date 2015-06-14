359be95f2e61473283b629c3868bd36a64c019a9
szimano <szimano@szimano.org>
2015-06-14 14:30:24 +0200

    dojrzewatr and butelkatr feed endpoints

--- src/main/groovy/pl/devoxx/prezentatr/feed/FeedController.groovy
+++ src/main/groovy/pl/devoxx/prezentatr/feed/FeedController.groovy
@@ -5,6 +5,7 @@ import com.wordnik.swagger.annotations.ApiOperation
 import groovy.transform.TypeChecked
 import groovy.util.logging.Slf4j
 import org.springframework.http.HttpEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import pl.devoxx.prezentatr.config.Collaborators
@@ -20,13 +21,23 @@ import static pl.devoxx.prezentatr.config.Versions.PREZENTATR_JSON_VERSION_1
 @Api(value = "feed", description = "Collects changes in the brewing states")
 class FeedController {
 
-//    @RequestMapping(
-//            value = "/dojrzewatr/{id}",
-//            produces = PREZENTATR_JSON_VERSION_1,
-//            consumes = PREZENTATR_JSON_VERSION_1,
-//            method = PUT)
-//    @ApiOperation(value = "sends an order to agregatr")
-//    public String dojrzewatr(@PathP) {
-//        log.info(body.body)
-//    }
+    @RequestMapping(
+            value = "/dojrzewatr/{id}",
+            produces = PREZENTATR_JSON_VERSION_1,
+            consumes = PREZENTATR_JSON_VERSION_1,
+            method = PUT)
+    @ApiOperation(value = "sends an order to agregatr")
+    public String dojrzewatr(@PathVariable String id) {
+        log.info("new dojrzewatr id: ${id}")
+    }
+
+    @RequestMapping(
+            value = "/butelkatr/{id}",
+            produces = PREZENTATR_JSON_VERSION_1,
+            consumes = PREZENTATR_JSON_VERSION_1,
+            method = PUT)
+    @ApiOperation(value = "sends an order to butelkatr")
+    public String butelkatr(@PathVariable String id) {
+        log.info("new butelkatr id: ${id}")
+    }
 }
